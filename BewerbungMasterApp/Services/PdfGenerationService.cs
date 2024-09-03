b66b87a2dd8a7036bb1f33a908c8f79f641eed18
mikolaj_kosmalski <mikolaj.kosmalski@eduswiper.com>
2024-09-03 19:57:54 +0200

    pdf generation update

--- BewerbungMasterApp/Services/PdfGenerationService.cs
+++ BewerbungMasterApp/Services/PdfGenerationService.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using PdfSharp.Pdf;
 using PdfSharp.Drawing;
@@ -6,84 +7,113 @@ using PdfSharp.Drawing.Layout;
 using BewerbungMasterApp.Interfaces;
 using BewerbungMasterApp.Models;
 
-namespace BewerbungMasterApp.Services
+namespace BewerbungMasterApp.Services;
+
+public class PdfGenerationService : IPdfGenerationService
 {
-    public class PdfGenerationService : IPdfGenerationService
+    public async Task GenerateCoverLetterPdfAsync(string targetDirectoryPath, string fileName, User user, JobApplication application)
     {
-        public async Task GenerateCoverLetterPdfAsync(string targetDirectoryPath, string fileName, User user, JobApplication application)
+        var pdfFilePath = Path.Combine(targetDirectoryPath, fileName);
+
+        using var document = new PdfDocument();
+        document.Info.Title = "Cover Letter";
+
+        var page = document.AddPage();
+        using var gfx = XGraphics.FromPdfPage(page);
+        var headerFont = new XFont("Verdana", 12);
+        var bodyFont = new XFont("Verdana", 12);
+
+        var marginLeft = XUnit.FromCentimeter(2).Point;
+        var marginRight = XUnit.FromCentimeter(2).Point;
+        var marginTop = XUnit.FromCentimeter(2).Point;
+
+        var tf = new XTextFormatter(gfx);
+
+        // User details
+        DrawUserDetails(tf, headerFont, marginLeft, marginTop, user);
+
+        // Company details
+        DrawCompanyDetails(tf, headerFont, marginLeft, marginTop, application);
+
+        // Date
+        tf.DrawString($"{user.City}, {DateTime.Now:dd.MM.yyyy}", headerFont, XBrushes.Black,
+            new XRect(marginLeft, marginTop + 200, page.Width.Point - marginLeft - marginRight, 20), XStringFormats.TopLeft);
+
+        // Application title
+        var applicationTitle = application.Position == "Initiativbewerbung"
+            ? application.Position
+            : $"Bewerbung als {application.Position}";
+        tf.DrawString(applicationTitle, headerFont, XBrushes.Black,
+            new XRect(marginLeft, marginTop + 240, page.Width.Point - marginLeft - marginRight, 20), XStringFormats.TopLeft);
+
+        // Application content
+        var content = GenerateApplicationContent(application.Company, application.Position, user);
+        DrawContentWithParagraphs(tf, bodyFont, marginLeft, marginTop + 280, page.Width.Point - marginLeft - marginRight, page.Height.Point - marginTop, content);
+
+        document.Save(pdfFilePath);
+
+        await Task.CompletedTask;
+    }
+
+    private static void DrawUserDetails(XTextFormatter tf, XFont font, double marginLeft, double marginTop, User user)
+    {
+        var userDetails = new[]
         {
-            // Define the PDF file path
-            var pdfFilePath = Path.Combine(targetDirectoryPath, fileName);
+            $"{user.FirstName} {user.LastName}",
+            user.Address,
+            $"{user.ZipCode} {user.City}",
+            $"Email: {user.Email}",
+            $"LinkedIn: {user.LinkedIn}",
+            $"Phone: {user.Phone}"
+        };
 
-            // Create a new PDF document
-            using (var document = new PdfDocument())
-            {
-                document.Info.Title = "Cover Letter";
-
-                // Add a page to the document
-                PdfPage page = document.AddPage();
-                XGraphics gfx = XGraphics.FromPdfPage(page);
-                XFont headerFont = new XFont("Verdana", 12);
-                XFont bodyFont = new XFont("Verdana", 12);
-
-                double marginLeft = XUnit.FromCentimeter(1).Point;
-                double marginTop = XUnit.FromCentimeter(1).Point;
-
-                // Create a text formatter for text wrapping
-                XTextFormatter tf = new XTextFormatter(gfx);
-
-                // Add user details
-                tf.DrawString($"{user.FirstName} {user.LastName}", headerFont, XBrushes.Black, new XRect(marginLeft, marginTop, page.Width.Point - marginLeft * 2, page.Height.Point), XStringFormats.TopLeft);
-                tf.DrawString(user.Address, headerFont, XBrushes.Black, new XRect(marginLeft, marginTop + 20, page.Width.Point - marginLeft * 2, page.Height.Point), XStringFormats.TopLeft);
-                tf.DrawString($"{user.ZipCode} {user.City}", headerFont, XBrushes.Black, new XRect(marginLeft, marginTop + 40, page.Width.Point - marginLeft * 2, page.Height.Point), XStringFormats.TopLeft);
-                tf.DrawString($"Email: {user.Email}", headerFont, XBrushes.Black, new XRect(marginLeft, marginTop + 60, page.Width.Point - marginLeft * 2, page.Height.Point), XStringFormats.TopLeft);
-                tf.DrawString($"LinkedIn: {user.LinkedIn}", headerFont, XBrushes.Black, new XRect(marginLeft, marginTop + 80, page.Width.Point - marginLeft * 2, page.Height.Point), XStringFormats.TopLeft);
-                tf.DrawString($"Phone: {user.Phone}", headerFont, XBrushes.Black, new XRect(marginLeft, marginTop + 100, page.Width.Point - marginLeft * 2, page.Height.Point), XStringFormats.TopLeft);
-
-                tf.DrawString("An:", headerFont, XBrushes.Black, new XRect(marginLeft, marginTop + 140, page.Width.Point - marginLeft * 2, page.Height.Point), XStringFormats.TopLeft);
-                tf.DrawString(application.Company, headerFont, XBrushes.Black, new XRect(marginLeft, marginTop + 160, page.Width.Point - marginLeft * 2, page.Height.Point), XStringFormats.TopLeft);
-
-                tf.DrawString($"{user.City}, {DateTime.Now.ToString("dd.MM.yyyy")}", headerFont, XBrushes.Black, new XRect(marginLeft, marginTop + 200, page.Width.Point - marginLeft * 2, page.Height.Point), XStringFormats.TopLeft);
-
-                // Add application details
-                string applicationTitle = application.Position == "Initiativbewerbung"
-                    ? application.Position
-                    : $"Bewerbung als {application.Position}";
-
-                tf.DrawString(applicationTitle, headerFont, XBrushes.Black, new XRect(marginLeft, marginTop + 240, page.Width.Point - marginLeft * 2, page.Height.Point), XStringFormats.TopLeft);
-
-                // Generate the content of the application letter
-                string content = GenerateApplicationContent(application.Company, application.Position, user);
-
-                // Draw the content with text wrapping
-                tf.DrawString(content, bodyFont, XBrushes.Black, new XRect(marginLeft, marginTop + 280, page.Width.Point - marginLeft * 2, page.Height.Point - marginTop), XStringFormats.TopLeft);
-
-                // Save the document to file
-                document.Save(pdfFilePath);
-            }
-
-            await Task.CompletedTask;
-        }
-
-        // Helper method to generate application content
-        private string GenerateApplicationContent(string company, string position, User user)
+        for (int i = 0; i < userDetails.Length; i++)
         {
-            // Dynamically create the content based on the position
-            string positionText = position == "Initiativbewerbung"
-                ? $"bei {company}. "
-                : $"um die Position {position} bei {company}. ";
-
-            // Build the full content from user data
-            string content =
-                user.ApplicationText.Introduction +
-                user.ApplicationText.PositionIntro + positionText +
-                user.ApplicationText.Education +
-                user.ApplicationText.Experience +
-                user.ApplicationText.Skills +
-                user.ApplicationText.InternshipOffer +
-                user.ApplicationText.Closing;
-
-            return content;
+            tf.DrawString(userDetails[i], font, XBrushes.Black,
+                new XRect(marginLeft, marginTop + i * 20, 400, 20), XStringFormats.TopLeft);
         }
+    }
+
+    private static void DrawCompanyDetails(XTextFormatter tf, XFont font, double marginLeft, double marginTop, JobApplication application)
+    {
+        tf.DrawString("An:", font, XBrushes.Black,
+            new XRect(marginLeft, marginTop + 140, 400, 20), XStringFormats.TopLeft);
+        tf.DrawString(application.Company, font, XBrushes.Black,
+            new XRect(marginLeft, marginTop + 160, 400, 20), XStringFormats.TopLeft);
+    }
+
+    private static void DrawContentWithParagraphs(XTextFormatter tf, XFont font, double x, double y, double width, double height, string content)
+    {
+        var paragraphs = content.Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+        var yOffset = y;
+
+        foreach (var paragraph in paragraphs)
+        {
+            tf.DrawString(paragraph, font, XBrushes.Black, new XRect(x, yOffset, width, height), XStringFormats.TopLeft);
+            yOffset += 30; // Add space between paragraphs
+        }
+    }
+
+    private static string GenerateApplicationContent(string company, string position, User user)
+    {
+        var positionText = position == "Initiativbewerbung"
+            ? $"bei {company}. "
+            : $"um die Position {position} bei {company}. ";
+
+        return $"""
+            {user.ApplicationText.Introduction}
+
+            {user.ApplicationText.PositionIntro}{positionText}
+
+            {user.ApplicationText.Education}
+
+            {user.ApplicationText.Experience}
+
+            {user.ApplicationText.Skills}
+
+            {user.ApplicationText.InternshipOffer}
+
+            {user.ApplicationText.Closing}
+            """;
     }
 }
